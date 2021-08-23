3c04237a45e601fb6ad4f4d4a3f7bf79c6c2911a
Aziz Mohamed <aziz@azizmohamed.com>
2021-08-23 14:46:26 +1000

    updates

--- meshui/Pages/Index.cshtml.cs
+++ meshui/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace meshui.Pages
@@ -11,15 +12,19 @@ namespace meshui.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public string MeshAppUrl { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            MeshAppUrl = Configuration["MeshAppUrl"];
         }
     }
 }
--- meshui/Startup.cs
+++ meshui/Startup.cs
@@ -45,7 +45,7 @@ namespace meshui
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
